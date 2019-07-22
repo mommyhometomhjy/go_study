24943e764771d10583e5e60d2a049d2ec26988de
Tom Huang <15669567489@163.com>
2019-07-22 17:54:24 +0800

    创建包

--- go程序语言设计/CH2/tempconv/tempconv.go
+++ go程序语言设计/CH2/tempconv/tempconv.go
@@ -11,9 +11,10 @@ const (
 	BoilingC      Celsius = 100
 )
 
-func CToF(c Celsius) Fahrenheit {
-	return Fahrenheit(c*9/5 + 32)
+func (c Celsius) String() string {
+	return fmt.Sprintf("%g c", c)
 }
-func FToC(f Fahrenheit) Celsius {
-	return Celsius((f - 32) * 5 / 9)
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%g f", f)
 }
